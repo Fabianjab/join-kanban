969abce5c7057f71d0198fa128bdffd4dffa884c
Fabianjab <fabian.jablonka@web.de>
2023-08-10 16:05:35 +0200

    added-basic-content

--- js/help.js
+++ js/help.js
@@ -10,13 +10,13 @@ function renderHelp() {
         </div>
         <h2 class="h2_help">Help</h2>
         <span id="what_is_join" class="small_headlines_help">What is Join?</span>
-        <span id="introduction_join" class="span_help" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.
+        <span id="introduction_join" class="span_help" >Join is the ultimate Kanban management tool, empowering teams to streamline workflows, visualize tasks, and enhance productivity. With its intuitive interface, customizable boards, and collaborative features, Tools transforms your work processes into a smooth, visual experience. Effortlessly track tasks, assign responsibilities, and monitor progress, all in one platform. Whether you're a small team or a large enterprise, Tools adapts to your needs, helping you achieve seamless project management. Say goodbye to complexity and hello to simplicity with Tools, your go-to Kanban companion for optimized efficiency and success.
         </span>
         <span id="how_to_use_it" class="small_headlines_help">How to use it</span
     <ol>
-        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris.</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna.</li>
-        <li>Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.</li>
+        <li>On the board page, you can view and modify the current status of projects. Use drag and drop to move ongoing projects to the latest project status.</li>
+        <li>On the 'Add Task' page, you can create tasks. You have the option to add collaborating team members and set a deadline. Additionally, you can insert subtasks.</li>
+        <li>The 'Contact' page provides an overview of all contacts working in the Join tool. Here, you'll find information such as phone numbers and email addresses of all individuals.</li>
     </ol>
     </div>
     `
--- js/legal_notice.js
+++ js/legal_notice.js
@@ -9,11 +9,18 @@ function renderLegal() {
             <img class="arrow_help" src="img/black_arrow_back.svg">
         </div>
         <h2 class="h2_help">Legal Notice</h2>
-        <span id="what_is_join" class="small_headlines_help">Subtitle</span>
-        <span id="introduction_join" class="span_help" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.
+        <span id="what_is_join" class="small_headlines_help">Informations</span>
+        <span id="introduction_join" class="span_help" >The information provided on this website is for general purposes only. While we strive to keep the information up to date and accurate, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability of the website or the information, products, services, or related graphics contained on the website. Any reliance you place on such information is therefore strictly at your own risk. In no event will we be liable for any loss or damage, including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+
+
         </span>
-        <span id="how_to_use_it" class="small_headlines_help">Subtitle</span
-        <span id="introduction_join" class="span_help" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.
+        <span id="how_to_use_it" class="small_headlines_help">Good to know</span
+        <span id="introduction_join" class="span_help" >The contents of this website are intended for informational purposes only and do not constitute legal advice. The use of this website does not establish a lawyer-client relationship. While we make efforts to provide accurate and up-to-date information, we make no representations or warranties regarding the accuracy, completeness, or suitability of the information contained herein. Any reliance on the information provided on this website is at your own risk. We disclaim all liability for any actions taken based on the information presented. Always seek the advice of a qualified legal professional before making any legal decisions.
+
+
+
+
+
 
         
     </div>
